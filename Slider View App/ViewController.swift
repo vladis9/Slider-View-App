0138f4132858e8fbffffd6422bd54acb0c5cf9ad
Vlad Zinevich <vladisznv@gmail.com>
2021-05-12 20:05:59 +0300

    Initial Commit

---
+++ Slider View App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider View App
+//
+//  Created by Vlad Zinevich on 12.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
