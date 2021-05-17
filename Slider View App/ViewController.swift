fdc8e11dfb6b93b8ed2981108c1b8217edaf339f
Vlad Zinevich <vladisznv@gmail.com>
2021-05-17 16:09:10 +0300

    add correctly values from slider

--- Slider View App/ViewController.swift
+++ Slider View App/ViewController.swift
@@ -9,12 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var colorView: UIView!
+
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
+    @IBOutlet var textFieldRed: UITextField!
+    @IBOutlet var textFieldGreen: UITextField!
+    @IBOutlet var textFieldBlue: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 15
+        
+        sliderRed.tintColor = .red
+        sliderGreen.tintColor = .green
     }
-
-
+    
+    // change color with sliders
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        switch sender.tag {
+        case 0:
+            labelRed.text = roundingValues(from: sender)
+            textFieldRed.text = roundingValues(from: sender)
+        case 1:
+            labelGreen.text = roundingValues(from: sender)
+            textFieldGreen.text = roundingValues(from: sender)
+        case 2:
+            labelBlue.text = roundingValues(from: sender)
+            textFieldBlue.text = roundingValues(from: sender)
+        default:
+            break
+        }
+    }
+    
+    // rounding values
+    private func roundingValues(from slider: UISlider) -> String {
+        return String(format: "%.2f", slider.value)
+    }
 }
 
